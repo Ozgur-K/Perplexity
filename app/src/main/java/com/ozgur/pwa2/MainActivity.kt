adcd315e33ba8c884376a833759874b498ff90d4
Ozgur <evrenater@gmail.com>
2025-05-16 11:45:00 +0300

    edit 1

--- app/src/main/java/com/ozgur/pwa2/MainActivity.kt
+++ app/src/main/java/com/ozgur/pwa2/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ozgur.pwa.ui.theme.CoPilotTheme
+import com.ozgur.pwa2.ui.theme.CoPilotTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
