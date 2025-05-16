b00ce7b6ba2f29daad1d1dec43b52dd8aaab9b16
Ozgur <evrenater@gmail.com>
2025-05-16 11:06:33 +0300

    Initial commit

---
+++ app/src/main/java/com/ozgur/pwa2/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.ozgur.pwa2
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ozgur.pwa.ui.theme.CoPilotTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val url = "https://www.perplexity.ai/"
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(url))
+
+        // Finish the activity so the user only sees the Chrome Custom Tab
+        finish()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CoPilotTheme {
+        Greeting("Android")
+    }
+}
